9bbe5515dff38bf4ae460c217db84d7e88ebd9b1
Bobby Cannon <bobby.cannon@live.com>
2021-08-11 09:20:06 -0400

    upgrade dependencies

--- Scripts/Invoke-SprotoDebugging.ps1
+++ Scripts/Invoke-SprotoDebugging.ps1
@@ -10,8 +10,8 @@ $ErrorActionPreference = "STOP"
 
 Clear-Host
 
-$versionFull = "3.0.5.0"
-$version = "3.0.5"
+$versionFull = "3.0.6.0"
+$version = "3.0.6"
 $files = Get-ChildItem $ProjectPath *.csproj -Recurse | Select-Object Fullname
 
 $sprotoR = "<Reference Include=`"Sproto, Version=$($versionFull), Culture=neutral, processorArchitecture=MSIL`"><HintPath>..\packages\Sproto.$version\lib\netstandard2.0\Sproto.dll</HintPath></Reference>"
